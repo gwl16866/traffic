47b78499a8488817f87dbbb20dbfcb2f00867b65
gwl <1079677424@qq.com>
2020-08-03 17:49:43 +0800

    高伟立

--- src/components/page/index/xyparticulars.vue
+++ src/components/page/index/xyparticulars.vue
@@ -16,29 +16,32 @@
       prop="headImg"
       label="头像"
       width="180">
+              <template slot-scope="scope">
+          <img :src="scope.row.headImg" class="avatar">
+        </template>
     </el-table-column>
     <el-table-column
-      prop="realname"
+      prop="realName"
       label="真实姓名"
       width="180">
     </el-table-column>
     <el-table-column
-      prop="cardid"
+      prop="cardId"
       label="身份证号"
        width="180">
     </el-table-column>
       <el-table-column
-      prop="jobname"
+      prop="jobName"
       label="岗位名称"
         width="180">
     </el-table-column>
       <el-table-column
-      prop="busnum"
+      prop="busNum"
       label="车牌号码"
         width="180">
     </el-table-column>
       <el-table-column
-      prop="linknum"
+      prop="linkNum"
       label="联系电话"
       width="180">
     </el-table-column>
@@ -46,29 +49,97 @@
       prop="status"
       label="培训状态"
       width="180">
+       <template slot-scope="scope" >
+        <el-tag v-if="scope.row.astatus=='1'" type="danger">未合格</el-tag>
+        <el-tag v-else-if="scope.row.astatus=='2'" type="success">培训合格</el-tag>
+        <el-tag v-else-if="scope.row.astatus==null" type="danger">未开始</el-tag>
+       
+      </template>
     </el-table-column>
        <el-table-column
-      prop="联系电话"
+      prop="startTime"
       label="开始时间"
       width="180">
+         <template slot-scope="scope" >
+        <el-tag v-if="scope.row.startTime==null" type="danger">未开始</el-tag>
+        <el-tag v-else>{{scope.row.startTime}}</el-tag>
+
+
+      </template>
     </el-table-column>
        <el-table-column
       prop="score"
       label="考试分数"
       width="180">
+       <template scope="scope">                    
+                    <p v-if="scope.row.score==null">暂未考试</p>
+                    <p v-if="scope.row.score!=null">{{scope.row.score}}</p>             
+                </template>
     </el-table-column>
        <el-table-column
       prop="联系电话"
       label="考试详情"
       width="180">
+       <template slot-scope="scope" >
+        <el-button
+        align="center"
+          size="mini"
+          
+          type="danger"
+          @click="scorexiangqing(scope.row.aid)">查看详情</el-button>
+      </template>
     </el-table-column>
-       <el-table-column
-      prop="联系电话"
-      label="操作"
+  </el-table>
+
+    
+<el-dialog
+  title="考试详情"
+  :visible.sync="dialogVisible"
+  width="70%"
+  :before-close="handleClose">
+  <span>
+
+ <el-table
+    :data="tableData2"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="headImg"
+      label="题型"
       width="180">
     </el-table-column>
+    <el-table-column
+      prop="realName"
+      label="题目"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="cardId"
+      label="选项"
+       width="180">
+    </el-table-column>
+      <el-table-column
+      prop="jobName"
+      label="已选答案"
+        width="180">
+    </el-table-column>
+         <el-table-column
+      prop="jobName"
+      label="正确答案"
+        width="180">
+    </el-table-column>
+    <el-table-column
+      prop="jobName"
+      label="判卷"
+        width="180">
+    </el-table-column>
+   
     
   </el-table>
+
+  </span>
+</el-dialog>
+
 
     </div></el-col>
 </el-row>
@@ -83,7 +154,8 @@ export default {
 data() {
         return {
           tableData:{},
-          id:""
+          id:"",
+           dialogVisible: false
         };
     }, 
      mounted(){
@@ -94,6 +166,7 @@ data() {
       methods: {
         tableinit(){
                var a=this;   
+             
       		this.$axios.get('http://localhost:8081/statistics/statisticsinfo/xiangqing2?id='+a.id)
 			  .then(function (response) {
                 a.tableData=response.data
@@ -103,6 +176,23 @@ data() {
 			    console.log(error);
 			  });
 
+
+
+
+        },
+        scorexiangqing(e){
+          var a=this;
+          if(e==null){
+            this.$message({
+          message: '未考试，暂无考试详情！',
+          type: 'warning'
+        });
+          }else{
+              a.dialogVisible = true
+
+
+
+          }
 
 
 
